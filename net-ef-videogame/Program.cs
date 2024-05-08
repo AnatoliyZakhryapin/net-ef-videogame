1d150edb2ad0e4645fddf55443acb09a259bff0a
Anatoliy Zakhryapin <anatoli.zakhryapin@gmail.com>
2024-05-08 19:46:46 +0200

    REFACTOR function CreateNewGame in Program

--- net-ef-videogame/Program.cs
+++ net-ef-videogame/Program.cs
@@ -164,90 +164,114 @@ namespace net_ef_videogame
 
             while (true)
             {
-                try
+                while (true)
+                {
+                    try
+                    {
+                        Console.WriteLine();
+                        Console.Write("Inserisci il nome del gioco: ");
+                        string inputName = Console.ReadLine();
+
+                        if (string.IsNullOrWhiteSpace(inputName))
+                            throw new Exception("Il nome non puo essere vuoto. Riprova");
+
+                        name = inputName;
+                        break;
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine($"Errore: {ex.Message}");
+                    }
+                }
+
+                while (true)
+                {
+                    try
+                    {
+                        Console.WriteLine();
+                        Console.Write("Inserisci il overview del gioco: ");
+                        string inputOverview = Console.ReadLine();
+
+                        if (string.IsNullOrWhiteSpace(inputOverview))
+                            throw new Exception("Overview non puo essere vuoto. Riprova");
+
+                        overview = inputOverview;
+                        break;
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine($"Errore: {ex.Message}");
+                    }
+                }
+
+                while (true)
+                {
+                    try
+                    {
+                        Console.WriteLine();
+                        Console.Write("Inserisci la data di rilascio del gioco (formato: yyyy-MM-dd): ");
+                        string inputReleaseDate = Console.ReadLine();
+
+                        if (!DateTime.TryParse(inputReleaseDate, out releaseDate))
+                            throw new Exception("Formato data non valido. Riprova.");
+
+                        break;
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine($"Errore: {ex.Message}");
+                    }
+                }
+
+                while (true)
+                {
+                    try
+                    {
+                        Console.WriteLine();
+                        Console.Write("Inserisci id di softwareHouseID: ");
+                        string inputSoftwareHouseID = Console.ReadLine();
+
+                        List<SoftwareHouse> softwareHouses = VideoGameManager.GetSoftwareHouseList();
+
+                        if (!long.TryParse(inputSoftwareHouseID, out long id))
+                            throw new Exception("Formato Id non valido. Riprova.");
+
+                        if (!softwareHouses.Any(sh => sh.SoftwareHouseId == id))
+                            throw new Exception("L'ID inserito non corrisponde a nessuna casa di software. Riprova.");
+
+                        softwareHouseID = id;
+                        break;
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine($"Errore: {ex.Message}");
+                    }
+                }
+
+                VideoGame newVideoGame = new VideoGame(name, overview, releaseDate, DateTime.Now, DateTime.Now, softwareHouseID);
+
+                if (VideoGameManager.AddNewGame(newVideoGame))
                 {
                     Console.WriteLine();
-                    Console.Write("Inserisci il nome del gioco: ");
-                    string inputName = Console.ReadLine();
-
-                    if (string.IsNullOrWhiteSpace(inputName))
-                        throw new Exception("Il nome non puo essere vuoto. Riprova");
-
-                    name = inputName;
+                    Console.WriteLine("VideoGame è stato inserito con successo!");
+                    Console.WriteLine();
+                    Console.WriteLine("VideoGame inserito è: ");
+                    Console.WriteLine();
+                    Console.WriteLine($"{newVideoGame.ToString()}");
+                    Console.WriteLine();
                     break;
                 }
-                catch (Exception ex)
+                else
                 {
                     Console.WriteLine();
-                    Console.WriteLine($"Errore: {ex.Message}");
+                    Console.WriteLine("Salvatoggio non e andato a buon fine, riprova.");
                 }
             }
-
-            while (true)
-            {
-                try
-                {
-                    Console.WriteLine();
-                    Console.Write("Inserisci il overview del gioco: ");
-                    string inputOverview = Console.ReadLine();
-
-                    if (string.IsNullOrWhiteSpace(inputOverview))
-                        throw new Exception("Overview non puo essere vuoto. Riprova");
-
-                    overview = inputOverview;
-                    break;
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine();
-                    Console.WriteLine($"Errore: {ex.Message}");
-                }
-            }
-
-            while (true)
-            {
-                try
-                {
-                    Console.WriteLine();
-                    Console.Write("Inserisci la data di rilascio del gioco (formato: yyyy-MM-dd): ");
-                    string inputReleaseDate = Console.ReadLine();
-
-                    if (!DateTime.TryParse(inputReleaseDate, out releaseDate))
-                        throw new Exception("Formato data non valido. Riprova.");
-
-                    break;
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine();
-                    Console.WriteLine($"Errore: {ex.Message}");
-                }
-            }
-
-            while (true)
-            {
-                try
-                {
-                    Console.WriteLine();
-                    Console.Write("Inserisci id do softwareHouseID: ");
-                    string inputSoftwareHouseID = Console.ReadLine();
-
-                    if (!long.TryParse(inputSoftwareHouseID, out long id))
-                        throw new Exception("Formato Id non valido. Riprova.");
-
-                    softwareHouseID = id;
-                    break;
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine();
-                    Console.WriteLine($"Errore: {ex.Message}");
-                }
-            }
-
-            VideoGame newVideoGame = new VideoGame(name, overview, releaseDate, DateTime.Now, DateTime.Now, softwareHouseID );
-
-            VideoGameManager.AddNewGame(newVideoGame);
+            
         }
 
         static void SearchGameById ()
