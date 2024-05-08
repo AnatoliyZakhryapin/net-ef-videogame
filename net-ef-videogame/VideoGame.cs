fb5909ce13f8c3f77f8fd9156a4062936d8bfa16
Anatoliy Zakhryapin <anatoli.zakhryapin@gmail.com>
2024-05-08 18:52:56 +0200

    REFACTOR Class VideoGame

--- net-ef-videogame/VideoGame.cs
+++ net-ef-videogame/VideoGame.cs
@@ -4,43 +4,42 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 
 namespace net_ef_videogame
 {
-    internal class VideoGame
+
+    [Table("videogames")]
+    public class VideoGame
     {
-        public long Id { get; private set; }
+        [Key] public long Id { get; private set; }
         public string Name { get; private set; }
         public string Overview { get; private set; }
         public DateTime ReleaseDate { get; private set; }
-        public DateTime CreatedAt { get; private set; }
-        public DateTime UpdatedAt { get; private set;}
-        public long SoftwareHouseID { get; private set; }
+        public DateTime? CreatedAt { get; private set; }
+        public DateTime? UpdatedAt { get; private set;}
 
-        public VideoGame (string name, string overview, DateTime releaseDate, DateTime createdAt, long softwareHouseID)
-        {
-            Name = name;
-            Overview = overview;
-            ReleaseDate = releaseDate;
-            CreatedAt = createdAt;
-            UpdatedAt = createdAt;
-            SoftwareHouseID = softwareHouseID;
-        }
+        //[ForeignKey("SoftwareHouse")]
+        public long SoftwareHouseId { get; set; }
+        public SoftwareHouse SoftwareHouse { get; set; }
 
-        public VideoGame(long id, string name, string overview, DateTime releaseDate, DateTime createdAt, DateTime updatedAt, long softwareHouseID)
+        public VideoGame() { }
+
+        public VideoGame(string name, string overview, DateTime releaseDate, DateTime? createdAt, DateTime? updatedAt, long softwareHouseID)
         {
-            Id = id;
+            //Id = id;
             Name = name;
             Overview = overview;
             ReleaseDate = releaseDate;
             CreatedAt = createdAt;
             UpdatedAt = updatedAt;
-            SoftwareHouseID = softwareHouseID;
+            SoftwareHouseId = softwareHouseID;
         }
 
         public override string ToString()
         {
-            return $"Id: {Id}\nName: {Name}\nOverview: {Overview}\nRelease date: {ReleaseDate}\nCreated at: {CreatedAt}\nUpdated at: {UpdatedAt}\nSoftware house id: {SoftwareHouseID}";
+            return $"Id: {Id}\nName: {Name}\nOverview: {Overview}\nRelease date: {ReleaseDate}\nCreated at: {CreatedAt}\nUpdated at: {UpdatedAt}\nSoftware house id: {SoftwareHouseId}";
         }
     }
 }
